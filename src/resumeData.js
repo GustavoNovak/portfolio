eb6f039fc19daf80e8fe22d49c823f250c233d43
Gustavo Novak <gustavoanovak@gmail.com>
2019-10-13 22:01:46 +0200

    Updating remote

--- src/resumeData.js
+++ src/resumeData.js
@@ -25,65 +25,133 @@ let resumeData = {
         "UniversityName":"Institut National des Sciences Appliquées de Rennes",
         "specialization":"Mechanical and Automation Engineering",
         "MonthOfPassing":"Aug",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "YearOfPassing":"2018"
       },
       {
         "UniversityName":"Universidade Federal do Paraná",
         "specialization":"Mechanical Engineering",
         "MonthOfPassing":"Jan",
-        "YearOfPassing":"2014",
-        "Achievements":"Some Achievements"
+        "YearOfPassing":"2014"
       }
     ],
     "work":[
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "CompanyName":"Civil and Mechanical Engineering Laboratory",
+        "specialization":"Mobile Robotics",
+        "MonthOfLeaving":"Sept",
+        "YearOfLeaving":"2019",
+        "Achievements":"Localization using computer vision and odometry ; terrain mapping and object localization ; robot navigation ; communication between computer and robot ; sensors support ; Graphical User Interface (GUI)"
       },
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
+        "CompanyName":"Novak contabilidade",
+        "specialization":"System Developer",
+        "MonthOfLeaving":"Aug",
         "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "Achievements":"Development of a system in PHP, MYSQL, ReactJs to automate the company management and also the development of some programs in VBA to optimize some processes."
+      },
+      {
+        "CompanyName":"UFPR Fórmula SAE",
+        "specialization":"Chief Engineer",
+        "MonthOfLeaving":"Jun",
+        "YearOfLeaving":"2015",
+        "Achievements":"Review the projects and analysis before execute them and I also took care of the global CAD to avoid conflicts. Another important aspect of my job was to give courses to participants about mechanical project. I also could learn about how to manage a project and how to work with designing in a complex assembly context. Thereafter, I could notice the importance of create a good documentation of how the projects was done."
+      },
+      {
+        "CompanyName":"UFPR Mechanical Engineering Junior Enterprise",
+        "specialization":"Consultant",
+        "MonthOfLeaving":"Mar",
+        "YearOfLeaving":"2014",
+        "Achievements":"Development of some products with my team, what provided me the possibility to train my skills in CAD (Solidworks), structural analysis (ANSYS) and I also could begin to learn how a company works. In addition, I could  give some courses to the participants about structural analysis, VBA and Solidworks."
       }
     ],
-    "skillsDescription":"Your skills here",
     "skills":[
       {
-        "skillname":"HTML5"
+        "skillname":"Catia v5", 
+        "skilllevel":"advanced"
       },
       {
-        "skillname":"CSS"
+        "skillname":"Solidworks", 
+        "skilllevel":"advanced"
       },
       {
-        "skillname":"Reactjs"
+        "skillname":"Ansys Structural", 
+        "skilllevel":"advanced"
+      },
+      {
+        "skillname":"Matlab", 
+        "skilllevel":"advanced"
+      },
+      {
+        "skillname":"Microsoft VBA", 
+        "skilllevel":"advanced"
+      },
+      {
+        "skillname":"PHP / Mysql / Javascript / ReactJs", 
+        "skilllevel":"advanced"
+      },
+      {
+        "skillname":"Portuguese", 
+        "skilllevel":"advanced"
+      },
+      {
+        "skillname":"French", 
+        "skilllevel":"intermediary"
+      },
+      {
+        "skillname":"English", 
+        "skilllevel":"basic"
       }
     ],
     "portfolio":[
       {
-        "name":"project1",
-        "description":"mobileapp",
+        "name":"2D drawing Formula",
+        "description":"UFPR Formula SAE 2017",
+        "imgurl":"images/portfolio/formulaGlobal2D.jpg"
+      },
+      {
+        "name":"Front upright 2D drawing",
+        "description":"UFPR Formula SAE 2017",  
+        "imgurl":"images/portfolio/frontUpright2D.jpg"
+      },
+      {
+        "name":"Torsional stiffness simulation",
+        "description":"UFPR Formula SAE 2017",
+        "imgurl":"images/portfolio/chassisSimulation.jpg"
+      },
+      {
+        "name":"Surgical Clipper",
+        "description":"UFPR Mechanical Engineering Junior Enterprise",
         "imgurl":"images/portfolio/clipador.png"
       },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/formulaGlobal2D.png"
+       {
+        "name":"Surgical Clipper 2D drawing",
+        "description":"UFPR Mechanical Engineering Junior Enterprise",
+        "imgurl":"images/portfolio/2DGlobal.jpg"
       },
       {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/frontUpright2D.png"
+        "name":"Folding Sack Truck render",
+        "description":"UFPR Mechanical Engineering Junior Enterprise",
+        "imgurl":"images/portfolio/caixaria.png"
       },
       {
-        "name":"project4",
+        "name":"Folding Sack Truck 2D drawing",
+        "description":"UFPR Mechanical Engineering Junior Enterprise",
+        "imgurl":"images/portfolio/caixaria2D.png"
+      },
+      {
+        "name":"Street Sweeper Car render",
         "description":"mobileapp",
-        "imgurl":"images/portfolio/chassisSimulation.jpg"
+        "imgurl":"images/portfolio/gari.png"
+      },
+      {
+        "name":"Street Sweeper Car 2D simulation",
+        "description":"UFPR Mechanical Engineering Junior Enterprise",
+        "imgurl":"images/portfolio/gari2D.png"
+      },
+      {
+        "name":"Street Sweeper Car 2D drawing",
+        "description":"UFPR Mechanical Engineering Junior Enterprise",
+        "imgurl":"images/portfolio/gariSimulation.png"
       }
     ],
     "testimonials":[
@@ -92,8 +160,8 @@ let resumeData = {
         "name":"Some technical guy"
       },
       {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
+        "description":"Gustavo is a good team worker. He won't stop until he got the excellence on a project.",
+        "name":"Daniella Novak"
       }
     ]
   }
